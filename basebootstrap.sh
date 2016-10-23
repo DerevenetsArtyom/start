ddc38e181b1cb66d89118a3ee9f80b942febd689
Arfey <misha.gavela@dataroot.co>
2016-10-24 01:30:50 +0300

    Deleted logs and corrected init

--- basebootstrap.sh
+++ basebootstrap.sh
@@ -61,7 +61,7 @@ from .local_settings import *
 
 sudo su - vagrant -c "source $VIRTUALENV_DIR/bin/activate && python $PROJECT_DIR/$PROJECT_NAME/manage.py migrate"
 
-mv $PROJECT_DIR/config/urls.py $PROJECT_DIR/$PROJECT_NAME/$PROJECT_DIR/urls.py
+mv $PROJECT_DIR/config/urls.py $PROJECT_DIR/$PROJECT_NAME/$PROJECT_NAME/urls.py
 
 
 # redis
