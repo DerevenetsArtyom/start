ac092ef772e2779f2a533c87e88ca2a75e80e466
Arfey <misha.gavela@dataroot.co>
2016-10-23 18:24:27 +0300

    Corrected b.sh

--- basebootstrap.sh
+++ basebootstrap.sh
@@ -44,14 +44,12 @@ cd settings
 
 ln -s $PROJECT_DIR/config/local_settings.py ./local.py
 
-echo "
+echo "DB_NAME='$PROJECT_NAME'
 try:
 	from local import *
 except ImportError:
 	from .base import *
 " > __init__.py
-
-echo "\nDB_NAME=$PROJECT_NAME" >> $PROJECT_DIR/config/local_settings.py
 
 sudo su - vagrant -c "source $VIRTUALENV_DIR/bin/activate && python $PROJECT_DIR/$PROJECT_NAME/manage.py migrate"
 
