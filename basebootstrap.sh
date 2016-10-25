b4974baecfc77eea4b6351f70279cad1962275e4
Arfey <misha.gavela@dataroot.co>
2016-10-25 14:03:24 +0300

    Corrected bootstrap.sh

--- basebootstrap.sh
+++ basebootstrap.sh
@@ -69,15 +69,14 @@ cat $PROJECT_DIR/makefile >> $PROJECT_DIR/newfile
 mv $PROJECT_DIR/newfile $PROJECT_DIR/makefile
 
 rm $PROJECT_DIR/init.sh $PROJECT_DIR/bootstrap.sh
-echo "PROJECT_NAME=$PROJECT_NAME" > bootstrap.sh
+echo "PROJECT_NAME=$PROJECT_NAME" > $PROJECT_DIR/bootstrap.sh
 cat $PROJECT_DIR/last_middle_bootstrap.sh >> $PROJECT_DIR/bootstrap.sh
 
-rm $PROJECT_DIR/last_middle_bootstrap.sh
+# rm $PROJECT_DIR/last_middle_bootstrap.sh
 
 sudo apt-get install -y gettext 
 
-cd $PROJECT_DIR/
-make messages
+make $PROJECT_DIR/messages
 
 
 # redis
