b6a80a14c3ece16d361fc9dff8706661312db118
Arfey <misha.gavela@dataroot.co>
2016-10-26 13:15:27 +0300

    Corrected sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,15 @@
-sudo apt-get install -y build-essential
 
+# declare paths
+DB_NAME=$PROJECT_NAME;
+VIRTUALENV_NAME=$PROJECT_NAME;
+PROJECT_DIR=/home/vagrant/$PROJECT_NAME;
+VIRTUALENV_DIR=/home/vagrant/$PROJECT_NAME/env;
+
+sudo apt-get install -y build-essential;
+sudo apt-get install -y git;
+sudo apt-get install -y gettext;
+
+# locale
 locale-gen en_US.UTF-8;
 export LANGUAGE=en_US.UTF-8;
 export LANG=en_US.UTF-8;
@@ -7,20 +17,16 @@ export LC_ALL=en_US.UTF-8;
 
 sudo dpkg-reconfigure locales --default-priority;
 
-DB_NAME=$PROJECT_NAME
-VIRTUALENV_NAME=$PROJECT_NAME
-
-PROJECT_DIR=/home/vagrant/$PROJECT_NAME
-VIRTUALENV_DIR=/home/vagrant/$PROJECT_NAME/env
-
 # Update Python
 sudo add-apt-repository ppa:fkrull/deadsnakes -y;
 sudo apt-get update;
 sudo apt-get install python3.5 python3-pip python3.5-dev -y;
-sudo apt-get install git -y;
-sudo apt-get install -y gettext 
+
 # Postgresql
 sudo apt-get install -y postgresql libpq-dev
+
+sudo -u postgres psql -c  "CREATE USER vagrant WITH PASSWORD 'vagrant';"
+sudo su postgres -c "createdb -E UTF8 -T template0 --locale=en_US.utf8 -O vagrant $PROJECT_NAME"
 
 # Virtual environment
 sudo pip3 install virtualenv;
@@ -47,14 +53,18 @@ cd settings
 ln -s $PROJECT_DIR/config/local.py ./local_settings.py
 mkdir $PROJECT_DIR/$PROJECT_NAME/logs
 
-echo "
-from .base import *
-from .local_settings import *
-" > __init__.py
+sed -i "1 i from .base import *" __init.py
+sed -i "1 i from .local_settings import *" __init.py
 
 sudo su - vagrant -c "source $VIRTUALENV_DIR/bin/activate && python $PROJECT_DIR/$PROJECT_NAME/manage.py migrate"
 
 mv $PROJECT_DIR/config/urls.py $PROJECT_DIR/$PROJECT_NAME/$PROJECT_NAME/urls.py
+sed -i "1 i NAMEDIR=$PROJECT_NAME" $PROJECT_DIR/makefile
+
+# create new bootstrap.sh
+rm $PROJECT_DIR/init.sh
+sed -i "1 i PROJECT_NAME=$PROJECT_NAME" $PROJECT_DIR/last_middle_bootstrap.sh
+mv $PROJECT_DIR/last_middle_bootstrap.sh $PROJECT_DIR/bootstrap.sh
 
 sudo su - vagrant -c "source $VIRTUALENV_DIR/bin/activate && cd $PROJECT_DIR/ && make messages"
 
@@ -63,6 +73,5 @@ sudo add-apt-repository ppa:chris-lea/redis-server -y
 sudo apt-get update
 sudo apt-get install -y redis-server
 
-# # Nodejs
-# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-# sudo apt-get install -y nodejs
+# install dependency
+sh $PROJECT_DIR/bootstrap_dependency.sh
